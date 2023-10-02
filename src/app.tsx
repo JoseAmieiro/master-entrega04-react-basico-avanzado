7ed67eb3ffdaa4e77cc1c6e2cb81afef5dbfa366
JoseAmieiro <joseperezamieiro@gmail.com>
2023-10-02 09:44:11 +0200

    Router file added

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./login";
-import { ListPage } from "./list";
-import { DetailPage } from "./detail";
 import { FilterContextProvider } from "./filter-context";
-
+import { AppRouter } from "./router";
 
 export const App = () => {
   return (
     <FilterContextProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/list" element={<ListPage />} />
-            <Route path="/detail/:id" element={<DetailPage />} />
-          </Routes>
-        </Router>
+        <AppRouter/>
     </FilterContextProvider>
   );
 };
