c6802a17dd4f051240a1d85277638659254aa8ae
JoseAmieiro <joseperezamieiro@gmail.com>
2023-10-02 10:59:05 +0200

    deatil completed

--- src/detail.tsx
+++ src/detail.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 
-export const DetailPage: React.FC = () => {
-  const {id} = useParams();
+interface MemberDetail {
+  id: string;
+  name: string;
+  login: string;
+  avatar_url: string
+}
 
-  return (
+
+
+export const DetailPage: React.FC = () => {
+  const { id } = useParams<{id: string}>();
+  const [member, setMember] = React.useState<MemberDetail>();
+
+  React.useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+    .then(r => r.json())
+    .then(setMember);
+  }, [])
+
+  return member ? (
     <>
       <h2>Hello from Detail page</h2>
-      <h3>User Id: {id}</h3>
+      <h3>{member.login}</h3>
+        <img src={member.avatar_url} alt= {member.login} width="150px"/>
+        <p>Id: {member.id}</p>
+        <p>Name: {member.name} </p>
       <Link to="/list">Back to list page</Link>
     </>
+  ) : (
+      <p>loading...</p>
   );
 };
